f79ca01f500cd4699701b6985a68fa8c1ee019c5
Paula Andrea <paula212.adso@gmail.com>
2026-09-19 19:50:36 -0500

    Delete src directory

--- src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registroCliente, login } = require('../controllers/authController');
-
-router.post('/registro-cliente', registroCliente); // RF01: autoregistro
-router.post('/login', login); // RF01 / RF02: login
-
-module.exports = router;
